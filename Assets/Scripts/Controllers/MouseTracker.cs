ecba2f7a721227a702f71b5166435c012a9e69e5
Marc King <marc.king@gmail.com>
2016-11-12 02:20:51 -0500

    Movement behaviours testing rig.

---
+++ Assets/Scripts/Controllers/MouseTracker.cs
@@ -0,0 +1,43 @@
+﻿// Marc King - mjking@umich.edu
+
+using UnityEngine;
+using System.Collections;
+
+public class MouseTracker : MonoBehaviour
+{
+
+    #region MonoBehavior Methods
+    private void Awake () { }
+    private void Start () { }
+    private void FixedUpdate () 
+    { 
+        
+    }
+    private void Update ()
+    { 
+        
+    }
+
+    private void OnGUI ()
+    {
+        transform.position = FindMousePosition();
+    }
+    private void LateUpdate () { }
+    private void OnDestroy () { }
+    #endregion MonoBehaviour Methods
+
+    private Vector3 FindMousePosition()
+    {
+        var plane = new Plane(Vector3.up, transform.position);
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance = 0.0f;
+
+        if (plane.Raycast(ray, out distance))
+        {
+            var location = ray.GetPoint(distance);
+            return location;
+        }
+        return Vector3.zero;
+    }
+
+}
