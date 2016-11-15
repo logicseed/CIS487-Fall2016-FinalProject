5dd439b35ba22b4f9b216413dad7c98a8eaa3fc2
Telfer <htelfer@UMICH.EDU>
2016-11-15 14:31:15 -0500

    Fixed rotations of some ship models, prefabs w/ turrets currently being redone

--- Assets/Scripts/Utilities/RotateToTarget.cs
+++ Assets/Scripts/Utilities/RotateToTarget.cs
@@ -9,7 +9,7 @@ public class RotateToTarget : MonoBehaviour {
 //#region MonoBehaviour Methods
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -20,18 +20,20 @@ public class RotateToTarget : MonoBehaviour {
 		transform.LookAt (targetPosition);
 		transform.Rotate (0,0,0);*/
 		if (Vector3.Distance (target.position, transform.position) <= range) {
-			float AngleRad = Mathf.Atan2 (target.position.x - transform.position.x, target.position.z - transform.position.z);
+			/*float AngleRad = Mathf.Atan2 (target.position.x - transform.position.x, target.position.z - transform.position.z);
 			float angle = (Mathf.Rad2Deg * AngleRad);
 			transform.rotation = Quaternion.AngleAxis (angle - 90, new Vector3 (0, 1, 0));
-			transform.Rotate (-90, 0, 0);
+			transform.Rotate (-90, 0, 0);*/
+			
 		} else {
 			//if(!transform.rotation.Equals(this.transform.parent)){
-				Vector3 targetDir = this.transform.parent.transform.position;
+				/*Vector3 targetDir = this.transform.parent.transform.position;
 				float step = 3 * Time.deltaTime;
 				Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
-				transform.rotation = Quaternion.LookRotation(newDir);
+				transform.rotation = Quaternion.LookRotation(newDir);*/
 			//}
 		}
+		transform.LookAt(target.position);
 
 
 	}
