cc363ad4df6cfd7dbd13ba362a11ba90ebb21dc3
Telfer <htelfer@UMICH.EDU>
2016-11-16 11:48:20 -0500

    Stalker and Ram fixed, turret array rotate script

---
+++ Assets/Scripts/Utilities/Simple Rotations/ArrayRotateToTarget.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+/**
++Attach to array of turrets (children of array gameobject art actual turrets)
+**/
+
+public class ArrayRotateToTarget : MonoBehaviour {
+
+	public GameObject target;
+	public int range = 15;
+	private Transform defaultPosition;
+//#region MonoBehaviour Methods
+	// Use this for initialization
+	void awake(){
+		defaultPosition=transform;
+	}
+
+	void Start () {
+		//defaultPosition=transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(Transform child in transform){
+			if(Vector3.Distance(target.transform.position,transform.position)<=range){
+				child.transform.LookAt(target.transform.position);
+			}
+			else {
+				transform.rotation=transform.parent.rotation;
+			}	
+		}
+		
+		
+
+
+	}
+}
+//#endregion MonoBehaviour Methods
---
+++ Assets/Scripts/Utilities/Simple Rotations/RotateToTarget.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateToTarget : MonoBehaviour {
+
+	public GameObject target;
+	public int range = 15;
+	private Transform defaultPosition;
+//#region MonoBehaviour Methods
+	// Use this for initialization
+	void awake(){
+		defaultPosition=transform;
+	}
+
+	void Start () {
+		//defaultPosition=transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(Transform child in transform){
+			if(Vector3.Distance(target.transform.position,transform.position)<=range){
+				transform.LookAt(target.transform.position);
+			}
+			else {
+				transform.rotation=transform.parent.rotation;
+			}	
+		}
+		
+		
+
+
+	}
+}
+//#endregion MonoBehaviour Methods
