e646fce1e6e869d027a165210d45d2f16d89f9ff
Marc King <marc.king@gmail.com>
2016-11-17 10:55:43 -0500

    Syncing my current workspace

---
+++ Assets/Scripts/Managers/AgentManager.cs
@@ -0,0 +1,28 @@
+﻿// Marc King - mjking@umich.edu
+
+using UnityEngine;
+using System.Collections;
+
+
+
+public class AgentManager : MonoBehaviour
+{
+    [Header("SDFDSFSDF")]
+    [HideInInspector]
+    public StandardMover mover;
+    [HideInInspector]
+    public TargetManager targets;
+
+    #region MonoBehavior Methods
+    private void Awake ()
+    {
+
+    }
+    private void Start () { }
+    private void FixedUpdate () { }
+    private void Update () { }
+    private void LateUpdate () { }
+    private void OnDestroy () { }
+    #endregion MonoBehaviour Methods
+
+}
