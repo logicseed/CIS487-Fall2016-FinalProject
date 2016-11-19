2354c687e96939687a783aac444a4eaa0cd356bb
Marc King <marc.king@gmail.com>
2016-11-18 22:18:59 -0500

    Planet Gallery

---
+++ Assets/Scripts/Movers/SimpleRotator.cs
@@ -0,0 +1,18 @@
+﻿// Marc King - mjking@umich.edu
+
+using UnityEngine;
+
+public class SimpleRotator : MonoBehaviour
+{
+    [Tooltip("Amount of rotation around each axis over time.")]
+    public Vector3 rotation;
+
+    #region MonoBehavior Methods
+
+    private void Update () 
+    { 
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+
+    #endregion MonoBehaviour Methods
+}
