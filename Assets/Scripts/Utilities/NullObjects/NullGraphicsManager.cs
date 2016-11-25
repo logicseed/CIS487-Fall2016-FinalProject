2431031c06601370719491cc600311feb6647d18
Marc King <marc.king@gmail.com>
2016-11-25 09:47:24 -0500

    Avoidance stress test.

--- Assets/Scripts/Utilities/NullObjects/NullGraphicsManager.cs
+++ Assets/Scripts/Utilities/NullObjects/NullGraphicsManager.cs
@@ -6,13 +6,6 @@ using System.Collections;
 public class NullGraphicsManager : GraphicsManager
 {
 
-    #region MonoBehavior Methods
-    private void Awake () { }
-    private void Start () { }
-    private void FixedUpdate () { }
-    private void Update () { }
-    private void LateUpdate () { }
-    private void OnDestroy () { }
-    #endregion MonoBehaviour Methods
+
 
 }
