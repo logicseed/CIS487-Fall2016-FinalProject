31fbeea4a8c303e26e77b74a9c1352a3431a3e3f
Telfer <htelfer@UMICH.EDU>
2016-11-28 16:37:42 -0500

    Generic Creep controller ver 1

---
+++ Assets/Scripts/Controllers/GenericCreepController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GenericCreepController : MonoBehaviour {
+	AgentManager agent;
+	GameObject seekTarget;
+	MovementBehaviour behavior ;
+	// Use this for initialization
+	void Start () {
+		agent.target.SetDirectTarget(seekTarget.GetComponent<AgentManager>());
+		behavior = new SeekBehaviour(agent.target.direct,true);
+	agent.mover.AddBehaviour(behavior);
+	}
+
+    private void setSeekTarget()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
