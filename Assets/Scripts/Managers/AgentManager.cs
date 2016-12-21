fe6854c5634ed78d2df64ffa3c64496707a39320
Marc King <marc.king@gmail.com>
2016-12-20 21:27:08 -0500

    Re syncvar agents

--- Assets/Scripts/Managers/AgentManager.cs
+++ Assets/Scripts/Managers/AgentManager.cs
@@ -8,10 +8,10 @@ using UnityEngine.Networking;
 public class AgentManager : NetworkBehaviour
 {
     [Tooltip("The type determines which components will be attached to this agent.")]
-    //[SyncVar]
+    [SyncVar]
     public AgentType type = AgentType.None;
     [Tooltip("The team determines how other agents respond to this agent.")]
-    //[SyncVar]
+    [SyncVar]
     public TeamType team = TeamType.World;
     [HideInInspector]
     public bool isPlayer = false;
